c6a0740ebf2b6f57daf84efeb813da0ec6b6c03c
Javier MM <javi@fortika>
2021-08-08 14:04:36 +0200

    Parse divisions

--- src/expression.rs
+++ src/expression.rs
@@ -20,7 +20,9 @@ pub enum Expression {
     Bind(String, Box<Expression>),
     Add(Vec<Expression>),
     Neg(Box<Expression>),
-    Prod(Vec<Expression>), //formula(String),
+    Prod(Vec<Expression>),
+    Div(Vec<Expression>),
+    //formula(String),
                            //algebra(String)
 }
 
@@ -32,7 +34,8 @@ impl Expression {
         for current in items {
             println!("current: {:?}", current);
 
-            if let Some(value) = Magnitude::get(current) {
+            if current == "" {
+            } else if let Some(value) = Magnitude::get(current) {
                 stack.push(Expression::Magnitude(value));
             } else if &current[0..1] == ":" {
                 let value = match stack.pop() {
@@ -50,6 +53,7 @@ impl Expression {
                         operator2(add_expression, &mut stack);
                     }
                     "*" => operator2(prod_expression, &mut stack),
+                    "/" => operator2(div_expression, &mut stack),
                     _ => {
                         stack.push(Expression::Variable(current.to_string()));
                     }
@@ -96,24 +100,25 @@ fn operator2(f: fn(Expression, Expression) -> Expression, stack: &mut Vec<Expres
 //------------------------------------------------
 
 fn add_expression(op1: Expression, op2: Expression) -> Expression {
-    match op1 {
-        Expression::Add(mut summands1) => match op2 {
-            Expression::Add(mut summands2) => {
-                summands1.append(&mut summands2);
-                Expression::Add(summands1)
-            }
-            _ => {
-                summands1.push(op2);
-                Expression::Add(summands1)
-            }
-        },
-        _ => match op2 {
-            Expression::Add(mut summands2) => {
-                summands2.insert(0, op1);
-                Expression::Add(summands2)
-            }
-            _ => Expression::Add(vec![op1, op2]),
-        },
+
+    // factors extraction
+    let mut operands1 = match op1 {
+        Expression::Add(operands) => operands,
+        _ => vec![op1]
+    };
+
+    let mut operands2 = match op2 {
+        Expression::Add(operands) => operands,
+        _ => vec![op2]
+    };
+
+    // operation
+    operands1.append(&mut operands2);
+
+    if operands1.len() == 1 {
+        operands1[0].clone()
+    } else {
+        Expression::Add(operands1)
     }
 }
 
@@ -131,27 +136,98 @@ fn neg_expression(op1: Expression) -> Expression {
     }
 }
 
+
 fn prod_expression(op1: Expression, op2: Expression) -> Expression {
-    match op1 {
-        Expression::Neg(expr1) => neg_expression(prod_expression(*expr1,op2)),
-        Expression::Prod(mut factors1) => match op2 {
-            Expression::Neg(expr2) => neg_expression(prod_expression(Expression::Prod(factors1), *expr2)),
-            Expression::Prod(mut factors2) => {
-                factors1.append(&mut factors2);
-                Expression::Prod(factors1.to_vec())
-            }
-            _ => {
-                factors1.push(op2);
-                Expression::Prod(factors1.to_vec())
-            },
-        },
-        _ => match op2 {
-            Expression::Neg(expr2) => neg_expression(prod_expression(op1,*expr2)),
-            Expression::Prod(mut factors2) => {
-                factors2.insert(0, op1);
-                Expression::Prod(factors2)
-            }
-            _ => Expression::Prod(vec![op1,op2])
-        },
+    let mut op1: Expression = op1;
+    let mut op2: Expression = op2;
+
+    // neg process
+    let mut neg : bool = false;
+
+    if let Expression::Neg(expr) = op1 {
+        neg = !neg ;
+        op1 = *expr;
+    }
+
+    if let Expression::Neg(expr) = op2 {
+        neg = !neg ;
+        op2 = *expr;
+    }
+
+    // operands extraction
+    let mut operands1 = match op1 {
+        Expression::Prod(operands) => operands,
+        _ => vec![op1]
+    };
+
+    let mut operands2 = match op2 {
+        Expression::Prod(operands) => operands,
+        _ => vec![op2]
+    };
+
+    // product
+    operands1.append(&mut operands2);
+
+    let expr_without_sign = if operands1.len() == 1 {
+        operands1[0].clone()
+    } else {
+        Expression::Prod(operands1)
+    };
+
+    // ending with sign
+    if !neg {
+        expr_without_sign
+    } else {
+        Expression::Neg(Box::new(expr_without_sign))
     }
 }
+
+fn div_expression(op1: Expression, op2: Expression) -> Expression {
+    let mut op1: Expression = op1;
+    let mut op2: Expression = op2;
+
+    // neg process
+    let mut neg : bool = false;
+
+    if let Expression::Neg(expr) = op1 {
+        neg = !neg ;
+        op1 = *expr;
+    }
+
+    if let Expression::Neg(expr) = op2 {
+        neg = !neg ;
+        op2 = *expr;
+    }
+
+    // factors extraction
+    let mut num1: Expression;
+    let mut den1: Expression = Expression::Prod(vec![]);
+    let mut num2: Expression;
+    let mut den2: Expression = Expression::Prod(vec![]);
+
+    if let Expression::Div(factors) = op1 {
+        num1 = factors[0].clone();
+        den1 = factors[1].clone();
+    } else {
+        num1 = op1;
+    }
+
+    if let Expression::Div(factors) = op2 {
+        num2 = factors[0].clone();
+        den2 = factors[1].clone();
+    } else {
+        num2 = op2;
+    }
+
+    // product
+    let num = prod_expression(num1,den2);
+    let den = prod_expression(den1,num2);
+
+    // ending with sign
+    if !neg {
+        Expression::Div(vec![num,den])
+    } else {
+        Expression::Neg(Box::new(Expression::Div(vec![num,den])))
+    }
+}
+
