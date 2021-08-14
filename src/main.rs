36941f55cda8b5e439af077efbacb007dab8614f
Javier M Mora-Merchan <jmmora@envio>
2021-08-14 17:59:37 +0200

    Dirty solution to show rendered equations

--- src/main.rs
+++ src/main.rs
@@ -129,8 +129,13 @@ fn preface(title: &str) -> Vec<String> {
 :: UserScripts [script]
 
 /* Import the mathjax library. */
-importScripts(\"https://polyfill.io/v3/polyfill.min.js?features=es6\");
-importScripts(\"https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js\");
+importScripts([
+    \"https://polyfill.io/v3/polyfill.min.js?features=es6\",
+    \"https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js\"
+]).then(() => {{
+        $(document).on(':passageinit', () => window.location.reload(false) );
+    }})
+    .catch(err => console.error(`MathJax load error: ${{err}}`));
 
 ",
         title,
